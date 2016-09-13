74b9fea602b2a6978be1723416b4e148d9aaf387
Tony Huinker <ahuinker@gmail.com>
2016-09-12 20:19:20 -0400

    initial version

--- resolver.go
+++ resolver.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/tonyHuinker/ehop"
 )
@@ -19,10 +21,36 @@ func askForInput(prompt string) string {
 	return strings.TrimSpace(response)
 }
 
+func changeDeviceName(myhop *ehop.EDA, newName string, oldName string, ID int, makeChange bool) {
+	if makeChange {
+		body := `{"custom_name": "` + newName + `"}`
+		_, err := ehop.CreateEhopRequest("PATCH", "devices/"+strconv.Itoa(ID), body, myhop)
+		if err != nil {
+			fmt.Println("Error making device update call")
+		} else {
+			fmt.Println("Updated device succesfully from " + oldName + " to " + newName)
+		}
+	} else {
+		fmt.Println("**Test** would have changed " + oldName + " to " + newName)
+	}
+}
+
 func main() {
+	var makeChanges = false
+	var slow = false
 	//Specify Key File
 	keyFile := askForInput("What is the name of your keyFile?")
 	myhop := ehop.NewEDAfromKey(keyFile)
+
+	ask := askForInput("Actually make changes (yes) or just a test run (test)?")
+	if ask == "yes" {
+		makeChanges = true
+	}
+
+	ask2 := askForInput("Make DNS requests safely (enter>>slow) or as fast as possible(enter>>fast)?")
+	if ask2 == "slow" {
+		slow = true
+	}
 
 	//Get all devices from the system
 	resp, _ := ehop.CreateEhopRequest("GET", "devices?active_from=-86400000", "null", myhop)
@@ -39,16 +67,14 @@ func main() {
 	//Grab all L3 devices... put into an array for the req.body
 	for _, device := range devices {
 		if device.IsL3 {
-			if device.DNSName != "" {
-				fmt.Println("Already has a name")
-			} else {
+			//fmt.Println("\nCustom Name: " + device.CustomName + "\nDefault Name: " + device.DefaultName + "\nDisplay Name: " + device.DisplayName + "\nDNS Name: " + device.DNSName + "\nDHCP Name: " + device.DhcpName)
+			if !((device.DNSName != "") && (device.DhcpName != "")) {
 				answers, err := net.LookupAddr(device.Ipaddr4)
-				if err != nil {
-					fmt.Println("DNS error")
-				} else {
-					for _, answer := range answers {
-						fmt.Println(answer)
+				if err == nil {
+					if slow {
+						time.Sleep(500 * time.Millisecond)
 					}
+					changeDeviceName(myhop, answers[0], device.DefaultName, device.ID, makeChanges)
 				}
 			}
 		}
